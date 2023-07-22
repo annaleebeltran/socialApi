dc28002ea64e6154e00a03cdc53446f50ce2f733
Annalee Beltran <annalee.beltranusaf@gmail.com>
2023-07-22 16:53:15 -0500

    added to models folder

---
+++ models/Thoughts.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reactions');
+const moment = require('moment');
+
+const ThoughtsSchema = new Schema(
+    {
+        thoughtText: {
+        type: String,
+        required: true,
+        minLength: 1,
+        maxLength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: [reactionSchema]
+},
+{
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+});
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', ThoughtsSchema);
+
+module.exports = Thought;
